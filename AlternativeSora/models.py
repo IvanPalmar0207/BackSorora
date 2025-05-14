78468319d46868a0bcae7dc57f051d31b1946efc
Ivan Palmar <palmar.ivan0205@gmail.com>
2025-05-14 17:53:49 -0500

    Author field Delete - Alternatives

--- AlternativeSora/models.py
+++ AlternativeSora/models.py
@@ -6,6 +6,5 @@ class AlternativeActions_tb(models.Model):
     
 class MediaAlternative_tb(models.Model):
     nameAlternative = models.CharField(max_length = 250, verbose_name = 'Name Alternative')
-    authorAlternative = models.CharField(max_length = 250, verbose_name = 'Author Alternative')    
     linkAlternative = models.TextField(verbose_name = 'Link Alternative')
     alternativeAction = models.ForeignKey(AlternativeActions_tb, on_delete = models.CASCADE)
